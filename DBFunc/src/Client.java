6bff7ba366aafe94a33e57dc271e033dc0adb3b8
PopeyesBiscuit <49940119+PopeyesBiscuit@users.noreply.github.com>
2020-09-22 12:01:07 +0200

    Update Client.java
    comments added

--- DBFunc/src/Client.java
+++ DBFunc/src/Client.java
@@ -2,17 +2,22 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.Socket;
 
+// Client class for connecting the robot to the laptop.
+
 public class Client {
 
+    // Static variable for socket init
     static Socket s;
 
+    //Client contructor for creating a client. The hostname  and port number are given through parameters
     public Client(String host, int port) throws IOException {
 
          s = new Socket(host, port);
         System.out.println("Connected to host");
 
     }
-
+     
+    // Code voor het verkrijgen van de X coordinaat via de Server-Client connectie
     public static int getDestX() {
         try{
 
@@ -31,6 +36,7 @@ public class Client {
         return 0;
     }
 
+     // Code voor het verkrijgen van de Y coordinaat via de Server-Client connectie
     public static int getDestY(){
 
         try{
